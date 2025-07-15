cb1b35738091cd794d6250e8ccf4125ec6f724ee
ksohlo <alabssouls@gmail.com>
2025-07-15 22:14:57 +0100

    updated designs

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,33 +1,60 @@
 
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 const Home = () => {
 	const [url, setUrl] = React.useState<string>('');
 	const navigate = useNavigate();
+	const testValidURL = (url: string) => {
+		try {
+			new URL(url);
+			return true;
+		} catch (error) {
+			return false;
+		}
+	};
+
+	const handleURL = () => {
+		if (!url || url === '') {
+			toast.error('Add a url to continue !!!');
+			return false;
+		}
+		const isValid = testValidURL(url);
+		if (isValid) {
+			navigate(`/shot?url=${url}`)
+		} else {
+			toast.error('Invalid URL');
+		}
+	}
+
   return (
     <div className='w-full h-screen bg-purple-200 overflow-hidden flex flex-col items-center justify-center'>
       <img src="/left.png" alt="left-top" className='h-2/3 absolute top-0 left-0'/>
       <img src="/right.png" alt="right-bottom" className='h-2/3 absolute bottom-0 right-0' />
 			{/* body */}
-			<div className='flex flex-col items-center'>
+			<div className='flex flex-col items-center z-10'>
 				<img src="/logo.png" className='w-14 h-14' alt="logo" />
 				<img src="/logo2.png" className='w-1/2 my-2' alt="logo" />
 				<p className='my-1 font-light text-sm w-2/5 text-center mx-auto'>Capture full websites in one click.</p>
 				<div className='w-full h-10 rounded-full bg-white overflow-hidden'>
 					<input type="url" name='url' placeholder='Enter website url here' onChange={(e) => {setUrl(e.target.value)}} className='w-full h-full p-2 font-light text-sm text-center focus:outline-none font-sans bg-white text-black placeholder:text-neutral-400'/>
 				</div>
-				<button onClick={() => {navigate(`/shot?url=${url}`)}} className='w-2/3 h-12 px-4 p-3 rounded-full border-2 border-white hover:shadow-lg hover:shadow-black duration-500 text-white text-sm font-medium bg-gradient-to-br from-purple-500 to-rose-500 my-5'>Start Capture</button>
+				<button
+					onClick={handleURL} 
+					className='w-2/3 h-12 p-3 rounded-full border-2 border-white hover:shadow-lg hover:shadow-neutral-500 duration-500 text-white text-sm font-medium bg-gradient-to-br from-purple-500 to-rose-500 my-5'>
+					Start Capture
+				</button>
 				<p className='font-semibold font-sans text-slate-500'>Don’t know ScreenFlow works? {'\t'}
-					<Link to='' className='bg-gradient-to-br from-purple-600 to-rose-600 text-transparent bg-clip-text hover:scale-105 duration-500 text-center flex justify-center'>
-					 Check How!
+					<Link to='/' className='bg-gradient-to-br from-purple-600 to-rose-600 text-transparent bg-clip-text hover:scale-105 duration-500 text-center flex justify-center'>
+						Check How!
 					</Link>
 				</p>
 			</div>
 			<div className='flex flex-row items-center gap-6 absolute bottom-3 left-1/2 -translate-x-1/2'>
-				<Link to={''} className='text-slate-500'>Privacy</Link>
-				<Link to={''} className='text-slate-500'>Terms</Link>
-				<Link to={''} className='text-slate-500'>Support</Link>
+				<Link to={'/'} className='text-slate-500'>Privacy</Link>
+				<Link to={'/'} className='text-slate-500'>Terms</Link>
+				<Link to={'/'} className='text-slate-500'>Support</Link>
 			</div>
     </div>
   )
--- client/src/pages/Shot.tsx
+++ client/src/pages/Shot.tsx
@@ -1,114 +1,133 @@
+import axios from "axios";
+import Lottie from "lottie-react";
 import React from "react";
 import { FaFile, FaTimesCircle } from "react-icons/fa";
-import { PiSpinnerGapBold } from "react-icons/pi";
+import { PiFileZipFill, PiSpinnerGapBold } from "react-icons/pi";
 import { RiCameraLensLine, RiTimerFill } from "react-icons/ri";
-import Switch from 'react-switch';
+import { useLocation, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import successAnim from "../assets/success.json";
 
 
 const Shot = () => {
-  const [isSwitchOn, setIsSwitchOn] = React.useState(false);
-  const [start, setStart] = React.useState(false)
-  const startCapture = () => {}
-  const endSession = () => {}
+  const [start, setStart] = React.useState(false);
+  const [finish, setFinish] = React.useState(false);
+  const location = useLocation();
+  const [url, setUrl] = React.useState('');
+  // const navigate = useNavigate();
   
+
+  const startCapture = async () => {
+    try {
+      setStart(true);
+      await axios.post('/screenshot', { url })
+      .then((res) => {
+        if (res.status === 200) {
+          
+        }
+        setStart(false);
+      })
+    } catch (error: any) {
+      setStart(false);
+      toast.error(String(error?.response?.message));
+    }
+  }
+  const endSession = () => {
+    try {
+      
+    } catch (error: any) {
+      toast.error(String(error?.response?.message));
+    }
+  }
+
+  
+  React.useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const urlFromParams = urlParams.get('url')
+    setUrl(urlFromParams as string);
+  },[location.search]);
+  // React.useEffect(() => {
+  //   if (url === '') {
+  //     navigate('/');
+  //   }
+  // },[location.search]);
   return (
     <div className="w-full h-screen">
-			<iframe src='https://www.9jawork.com/' className='w-full h-full mx-auto'>
-			</iframe>
-      {/* popup 1 desktop */}
-      {!start && <div className="z-50 absolute top-3 right-5 rounded-lg p-4 bg-white w-10/12 md:w-4/12 lg:w-3/12 shadow-lg shadow-neutral-500 hidden md:block">
-        <img src="/logo.png" alt="logo" className="w-8 h-8 mx-auto" />
-        <img src="/logo2.png" alt="logo" className="w-1/3 my-2 mx-auto" />
-        <div className="w-full grid grid-cols-2 gap-2">
-          <div className="w-full h-auto border-[1px] border-neutral-200 p-2 rounded-lg">
-            <FaFile size={18} className="text-rose-600" />
-            <p className="font-normal text-xs my-1">Pages</p>
-            <h2 className="font-black text-xl text-neutral-700 my-2">12</h2>
+			<div className="bg-purple-100 w-full h-screen flex flex-col items-center justify-center">
+        {/* popup 1 desktop */}
+        {(!start && !finish) && <div className="z-50 rounded-lg p-4 bg-white w-10/12 md:w-4/12 lg:w-3/12 shadow-lg shadow-neutral-500">
+            <img src="/logo.png" alt="logo" className="w-8 h-8 mx-auto" />
+            <img src="/logo2.png" alt="logo" className="w-1/3 my-2 mx-auto" />
+          <div className="w-full grid grid-cols-2 gap-2">
+            <div className="w-full h-auto border-[1px] border-neutral-200 p-2 rounded-lg">
+              <FaFile size={18} className="text-rose-600" />
+              <p className="font-normal text-xs my-1">Pages</p>
+              <h2 className="font-black text-xl text-neutral-700 my-2">12</h2>
+            </div>
+            <div className="w-full h-auto border-[1px] border-neutral-200 p-2 rounded-lg">
+              <RiTimerFill size={18} className="text-blue-600" />
+              <p className="font-normal text-xs my-1">Time to Capture</p>
+              <h2 className="font-black text-xl text-neutral-700 my-2">2 mins</h2>
+            </div>
           </div>
-          <div className="w-full h-auto border-[1px] border-neutral-200 p-2 rounded-lg">
-            <RiTimerFill size={18} className="text-blue-600" />
-            <p className="font-normal text-xs my-1">Time to Capture</p>
-            <h2 className="font-black text-xl text-neutral-700 my-2">2 mins</h2>
+          <div className="w-full h-10 my-4 border-[1px] border-neutral-200 rounded-full overflow-hidden">
+            <select className="text-sm p-2 w-full focus:outline-none">
+              <option value={''}>All View (Desktop & Mobile)</option>
+              <option value={''}>Desktop View only</option>
+              <option value={''}>Mobile View only</option>
+            </select>
           </div>
-        </div>
-        <div className="w-full h-10 my-4 border-[1px] border-neutral-200 rounded-full overflow-hidden">
-          <select className="text-sm p-2 w-full focus:outline-none">
-            <option value={''}>All View (Desktop & Mobile)</option>
-            <option value={''}>Desktop View only</option>
-            <option value={''}>Mobile View only</option>
-          </select>
-        </div>
-        <div className="flex flex-row items-center justify-between">
-          <p className="font-medium text-sm my-2">View the life capture</p>
-          <Switch 
-            onChange={setIsSwitchOn} 
-            checked={isSwitchOn} 
-            uncheckedIcon={<></>}
-            checkedIcon={<></>}
-          />
-        </div>
-        <button onClick={startCapture} className="text-white font-medium text-sm bg-gradient-to-br from-purple-500 to-rose-500 w-full p-3 my-4 rounded-full flex gap-2 items-center justify-center hover:shadow-md">
-          <RiCameraLensLine size={20} className="text-white" />
-          Begin Capture
-        </button>
-      </div>}
-      {/* popup 1 mobile */}
-      {!start && <div className="z-50 absolute bottom-3 right-1/2 translate-x-1/2 rounded-lg p-4 bg-white w-10/12 md:w-1/5 lg:w-2/10 shadow-lg shadow-neutral-500 block md:hidden">
-        <img src="/logo.png" alt="logo" className="w-8 h-8 mx-auto" />
-        <img src="/logo2.png" alt="logo" className="w-1/3 my-2 mx-auto" />
-        <div className="w-full grid grid-cols-2 gap-2">
-          <div className="w-full h-auto border-[1px] border-neutral-200 p-2 rounded-lg">
-            <FaFile size={18} className="text-rose-600" />
-            <p className="font-normal text-xs my-1">Pages</p>
-            <h2 className="font-black text-xl text-neutral-700 my-2">12</h2>
+          {/* <div className="flex flex-row items-center justify-between">
+            <p className="font-medium text-sm my-2">View the life capture</p>
+            <Switch 
+              onChange={setIsSwitchOn} 
+              checked={isSwitchOn} 
+              uncheckedIcon={<></>}
+              checkedIcon={<></>}
+            />
+          </div> */}
+          <button onClick={startCapture} className="text-white font-medium text-sm bg-gradient-to-br from-purple-500 to-rose-500 w-full p-3 my-4 rounded-full flex gap-2 items-center justify-center hover:shadow-md">
+            <RiCameraLensLine size={20} className="text-white" />
+            Begin Capture
+          </button>
+        </div>}
+        {/* popup 2 desktop*/}
+        {(start && !finish) && <div className="z-50 rounded-lg p-4 bg-white w-10/12 md:w-3/12 lg:w-2/12 shadow-lg shadow-neutral-500 hidden md:block">
+          <img src="/logo.png" alt="logo" className="w-8 h-8 mx-auto" />
+          <img src="/logo2.png" alt="logo" className="w-1/3 my-2 mx-auto" />
+          <button onClick={startCapture} className="text-white font-medium text-sm bg-gradient-to-br from-purple-400 to-rose-400 w-full p-3 my-4 rounded-full flex gap-2 items-center justify-center hover:shadow-md">
+            Capturing {'2/12'}
+            <PiSpinnerGapBold size={20} className="text-white animate-spin" />
+          </button>
+          <button onClick={endSession} className="text-xs flex items-center justify-center gap-1 mx-auto my-4 text-neutral-600"><FaTimesCircle size={10} className="text-neutral-600" /> End Session</button>
+        </div>}
+        {/* popup 3  */}
+        {finish && <div className="z-50 rounded-lg p-4 bg-white w-10/12 md:w-3/12 lg:w-3/12 shadow-lg shadow-neutral-500 hidden md:block">
+          <img src="/logo.png" alt="logo" className="w-8 h-8 mx-auto" />
+          <img src="/logo2.png" alt="logo" className="w-1/2 my-2 mx-auto" />
+          <div>
+            <Lottie animationData={successAnim} className="w-1/3 mx-auto my-3" />
+            {/* 2  */}
+            <div className="w-full grid grid-cols-2 gap-2">
+              <div className="w-full h-auto border-[1px] border-neutral-200 p-2 rounded-lg">
+                <FaFile size={18} className="text-rose-600" />
+                <p className="font-normal text-xs my-1">Pages</p>
+                <h2 className="font-black text-xl text-neutral-700 my-2">12</h2>
+              </div>
+              <div className="w-full h-auto border-[1px] border-neutral-200 p-2 rounded-lg">
+                <RiTimerFill size={18} className="text-blue-600" />
+                <p className="font-normal text-xs my-1">Time to Capture</p>
+                <h2 className="font-black text-xl text-neutral-700 my-2">2 mins</h2>
+              </div>
+            </div>
           </div>
-          <div className="w-full h-auto border-[1px] border-neutral-200 p-2 rounded-lg">
-            <RiTimerFill size={18} className="text-blue-600" />
-            <p className="font-normal text-xs my-1">Time to Capture</p>
-            <h2 className="font-black text-xl text-neutral-700 my-2">2 mins</h2>
-          </div>
-        </div>
-        <div className="w-full h-10 my-4 border-[1px] border-neutral-200 rounded-full overflow-hidden">
-          <select className="text-sm p-2 w-full focus:outline-none">
-            <option value={''}>All View (Desktop & Mobile)</option>
-            <option value={''}>Desktop View only</option>
-            <option value={''}>Mobile View only</option>
-          </select>
-        </div>
-        <div className="flex flex-row items-center justify-between">
-          <p className="font-medium text-sm my-2">View the life capture</p>
-          <Switch 
-            onChange={setIsSwitchOn} 
-            checked={isSwitchOn} 
-            uncheckedIcon={<></>}
-            checkedIcon={<></>}
-          />
-        </div>
-        <button onClick={startCapture} className="text-white font-medium text-sm bg-gradient-to-br from-purple-500 to-rose-500 w-full p-3 my-4 rounded-full flex gap-2 items-center justify-center hover:shadow-md">
-          <RiCameraLensLine size={20} className="text-white" />
-          Begin Capture
-        </button>
-      </div>}
-      {/* popup 2 desktop*/}
-      {start && <div className="z-50 absolute top-3 right-5 rounded-lg p-4 bg-white w-10/12 md:w-3/12 lg:w-2/12 shadow-lg shadow-neutral-500 hidden md:block">
-        <img src="/logo.png" alt="logo" className="w-8 h-8 mx-auto" />
-        <img src="/logo2.png" alt="logo" className="w-1/3 my-2 mx-auto" />
-        <button onClick={startCapture} className="text-white font-medium text-sm bg-gradient-to-br from-purple-400 to-rose-400 w-full p-3 my-4 rounded-full flex gap-2 items-center justify-center hover:shadow-md">
-          Capturing {'2/12'}
-          <PiSpinnerGapBold size={20} className="text-white animate-spin" />
-        </button>
-        <button onClick={endSession} className="text-xs flex items-center justify-center gap-1 mx-auto my-4 text-neutral-600"><FaTimesCircle size={10} className="text-neutral-600" /> End Session</button>
-      </div>}
-      {/* popup 2 mobile  */}
-      {start && <div className="z-50 absolute bottom-5 right-1/2 translate-x-1/2 rounded-lg p-4 bg-white w-2/3 md:w-1/5 lg:w-2/10 shadow-lg shadow-neutral-500 block md:hidden">
-        <img src="/logo.png" alt="logo" className="w-8 h-8 mx-auto" />
-        <img src="/logo2.png" alt="logo" className="w-1/3 my-2 mx-auto" />
-        <button onClick={startCapture} className="text-white font-medium text-sm bg-gradient-to-br from-purple-400 to-rose-400 w-full p-3 my-4 rounded-full flex gap-2 items-center justify-center hover:shadow-md">
-          Capturing {'2/12'}
-          <PiSpinnerGapBold size={20} className="text-white animate-spin" />
-        </button>
-        <button onClick={endSession} className="text-xs flex items-center justify-center gap-1 mx-auto my-4 text-neutral-600"><FaTimesCircle size={10} className="text-neutral-600" /> End Session</button>
-      </div>}
+            {/* 3  */}
+          <button onClick={() => {}} className="text-white font-medium mx-auto text-sm bg-gradient-to-br from-purple-500 to-rose-500 w-full shadow-md p-3 my-4 rounded-full flex gap-2 items-center justify-center hover:shadow-md">
+            <PiFileZipFill size={20} className="text-white" />
+            Download Zip
+          </button>
+        </div>}
+      </div>
     </div>
   )
 }
