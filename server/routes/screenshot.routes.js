e66f176c6c1726f8878f10b45b4e8a289ea5874e
Great <greatoladipupo818@gmail.com>
2025-07-15 14:15:10 +0100

    Update server configuration: add start script, enhance screenshot utility with additional launch options, and include new screenshot files

--- server/routes/screenshot.routes.js
+++ server/routes/screenshot.routes.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const { handleScreenshot } = require('../controllers/screenshot.controller');
 
 router.post('/screenshot', handleScreenshot);
-
+ 
 module.exports = router; 
--- server/utils/screenshot.js
+++ server/utils/screenshot.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 async function takeScreenshots(url, outDir) {
   if (!fs.existsSync(outDir)) fs.mkdirSync(outDir, { recursive: true });
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   const page = await browser.newPage();
   await page.goto(url, { waitUntil: 'networkidle0', timeout: 120000 });
   const filename = path.join(outDir, 'screenshot.png');
